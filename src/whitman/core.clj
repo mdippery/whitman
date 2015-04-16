fc681c01ea6b428877d0777c96d6d335a8c34b80
Michael Dippery <michael@monkey-robot.com>
2015-04-16 10:07:24 -0700

    Require path to configuration file as argument

--- src/whitman/core.clj
+++ src/whitman/core.clj
@@ -1,5 +1,12 @@
 (ns whitman.core
   (:gen-class))
 
+(defn exit [code msg]
+  (binding [*out* *err*]
+    (do (println msg)
+        (System/exit code))))
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (if (< (count args) 1)
+      (exit 1 "No configuration file specified")
+      (println "Hello, world!")))
