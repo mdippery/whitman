1c8745885fee5ca9b1d4cae1048498d533c35819
Michael Dippery <michael@monkey-robot.com>
2015-04-17 17:45:05 -0700

    Retrieve user agent from config

--- src/whitman/core.clj
+++ src/whitman/core.clj
@@ -10,7 +10,8 @@
 (defn -main [& args]
   (if (< (count args) 1)
       (exit 1 "No configuration file specified")
-      (-> (nth args 0)
-          slurp
-          json/read-str
-          println)))
+      (let [cfg (-> (nth args 0)
+                    slurp
+                    json/read-str)
+            user-agent (get cfg "user-agent")]
+        (println user-agent))))
