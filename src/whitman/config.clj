281b43881d7e24a69faf3f902c442949379321d3
Michael Dippery <michael@monkey-robot.com>
2015-04-17 19:21:38 -0700

    format-config: Make private

--- src/whitman/config.clj
+++ src/whitman/config.clj
@@ -2,7 +2,7 @@
   (:require [clojure.data.json :as json]
             [whitman.utils :as utils]))
 
-(defn format-config [cfg]
+(defn ^{:private true } format-config [cfg]
   (assoc cfg "user-agent" (format (get cfg "user-agent") utils/version)))
 
 (defn read-config [path]
