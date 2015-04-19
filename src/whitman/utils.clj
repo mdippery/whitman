8fc56501126cb17549e9393a52fd27d07795878e
Michael Dippery <michael@monkey-robot.com>
2015-04-18 21:33:40 -0700

    Get current time

--- src/whitman/utils.clj
+++ src/whitman/utils.clj
@@ -1,8 +1,12 @@
 (ns whitman.utils
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import [java.util Date]))
 
 (def version
   (if (.exists (io/as-file "project.clj"))
     (-> "project.clj" slurp read-string (nth 2))
     (-> (eval 'karmanaut.utils) .getPackage .getImplementationVersion)))
+
+(defn utcnow []
+  (Date.))
