58f924ed710923609105a8957d861fd7b0b4896e
Michael Dippery <michael@monkey-robot.com>
2015-04-21 13:54:30 -0700

    Crawl data

--- src/whitman/core.clj
+++ src/whitman/core.clj
@@ -1,5 +1,7 @@
 (ns whitman.core
-  (:require [whitman.config :as config])
+  (:require [whitman.config :as config]
+            [whitman.crawler :as crawler]
+            [whitman.db :as db])
   (:gen-class))
 
 (defn exit [code msg]
@@ -7,9 +9,14 @@
     (do (println msg)
         (System/exit code))))
 
+(defn do-crawl [cfg]
+  (let [users (crawler/records cfg)
+        docs (map #(crawler/sample-docs cfg %) users)]
+    (doseq [d docs] (db/insert cfg (:query d) (:insert d)))))
+
 (defn -main [& args]
   (if (< (count args) 1)
       (exit 1 "No configuration file specified")
       (-> (nth args 0)
           config/read-config
-          println)))
+          do-crawl)))
