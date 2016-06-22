25291b1bf910724124671ffa305b453c1862dfd1
Michael Dippery <michael@monkey-robot.com>
2016-06-22 10:45:04 -0700

    Upgrade to monger v2.1.0
    I can't go all the way to >= 3.0.0 due to some API changes, but this
    should do for now.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "2.2.0"]
-                 [com.novemberain/monger "2.0.0"]]
+                 [com.novemberain/monger "2.1.0"]]
   :main ^:skip-aot whitman.core
   :target-path "target/%s"
   :jar-name "whitman.jar"
