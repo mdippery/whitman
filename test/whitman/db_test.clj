4e7c7b2cb3d8c99406f43def42be4982ec8f0bcb
Michael Dippery <mdippery@ilm.com>
2017-03-16 10:32:35 -0700

    Use monkey-robot.com:27017 as MongoDB test URL
    Unlike whitman.monkey-robot.com, monkey-robot.com actually exists, so it
    won't throw an error when trying to connect.

--- test/whitman/db_test.clj
+++ test/whitman/db_test.clj
@@ -7,9 +7,9 @@
     (is (= (db/db-url cfg) "mongodb://localhost:27017/test"))))
 
 (deftest test-db-url-when-specified
-  (let [cfg {"connection" "whitman.monkey-robot.com:27017"
+  (let [cfg {"connection" "monkey-robot.com:27017"
              "database" "test"}]
-    (is (= (db/db-url cfg) "mongodb://whitman.monkey-robot.com:27017/test"))))
+    (is (= (db/db-url cfg) "mongodb://monkey-robot.com:27017/test"))))
 
 (deftest test-db-with-default
   (let [cfg {"database" "test"}]
@@ -20,7 +20,7 @@
         is)))
 
 (deftest test-db-when-specified
-  (let [cfg {"connection" "whitman.monkey-robot.com:27017"
+  (let [cfg {"connection" "monkey-robot.com:27017"
              "database" "test"}]
     (-> cfg
         db/db
